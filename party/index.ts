12d424df261bcfdff4a7d00fc88fe4f9a4b360cc
Avner Sorek <Avner.Sorek@TikalK.com>
2025-12-18 17:39:11 +0200

    fixes

--- party/index.ts
+++ party/index.ts
@@ -2,31 +2,35 @@ import type * as Party from "partykit/server";
 import { onConnect } from "y-partykit";
 
 const ROOMS_METADATA_ROOM_ID = "rooms";
-const PARTYKIT_HOST = 'http://localhost:1999';
-
-const createResponse = (body: object, status = 200) => new Response(JSON.stringify(body), {
-    status,
-    headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "http://localhost:5173", // Allow your frontend origin
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS", // Specify allowed methods
-        "Access-Control-Allow-Headers": "Content-Type", // Specify allowed headers
-    },
-});
 
 type RoomMetadata = {
     roomId: string;
 }
 
-const callRoomsMetadataRoom = (init: RequestInit) => fetch(`${PARTYKIT_HOST}/parties/main/${ROOMS_METADATA_ROOM_ID}`, init);
-
-// missing
-// connection count on rooms
-
 export default class SynthRoomServer implements Party.Server {
     constructor(readonly room: Party.Room) {
         console.log('Creating room handler', room.id);
     }
+
+    private getEnv(key: string, fallback: string) {
+        return this.room.env[key] ? String(this.room.env[key]) : fallback;
+    }
+
+    private callRoom = (roomId: string, init: RequestInit) =>
+        fetch(`${this.getEnv('PARTYKIT_HOST', "http://localhost:1999")}/parties/main/${roomId}`, init);
+
+    private callRoomsMetadataRoom = (init: RequestInit) =>
+        this.callRoom(ROOMS_METADATA_ROOM_ID, init);
+
+    private createResponse = (body: object, status = 200) => new Response(JSON.stringify(body), {
+        status,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": this.getEnv('CORS_ORIGIN', "http://localhost:5173"), // Allow your frontend origin
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS", // Specify allowed methods
+            "Access-Control-Allow-Headers": "Content-Type", // Specify allowed headers
+        },
+    });
 
     async onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
         // y-partykit handles all Yjs protocol communication
@@ -35,7 +39,7 @@ export default class SynthRoomServer implements Party.Server {
             callback: {
                 handler: async () => {
                     if (this.room.id !== ROOMS_METADATA_ROOM_ID) {
-                        await callRoomsMetadataRoom({
+                        await this.callRoomsMetadataRoom({
                             method: "POST",
                             body: JSON.stringify({
                                 roomId: this.room.id,
@@ -57,25 +61,25 @@ export default class SynthRoomServer implements Party.Server {
                 if (request.method === "POST") {
                     const payload: RoomMetadata = await request.json();
                     this.room.storage.put(payload.roomId, payload);
-                    return createResponse({});
+                    return this.createResponse({});
                 } else if (request.method === "GET") {
                     return await this.getRoomsList();
                 }
             } else {
                 // WE ARE IN THE REGULAR ROOMS
                 if (request.method === "GET") {
-                    return createResponse({
+                    return this.createResponse({
                         roomId: this.room.id,
                         connectionCount: [...this.room.getConnections()].length
                     });
                 }
             }
 
-            return createResponse({ error: "Not Found " + url.pathname }, 404);
+            return this.createResponse({ error: "Not Found " + url.pathname }, 404);
         } catch (error) {
             console.log(`Error on ${request.method} ${request.url}`);
             console.error(error);
-            return createResponse({ error: "Failed" }, 500);
+            return this.createResponse({ error: "Failed" }, 500);
         }
     }
 
@@ -83,11 +87,10 @@ export default class SynthRoomServer implements Party.Server {
         const roomsKeys = await this.room.storage.list().then(roomsMap => [...roomsMap.keys()]);
         const roomsList: RoomMetadata[] = [];
         for (const roomKey of roomsKeys) {
-            const roomData = await fetch(`${PARTYKIT_HOST}/parties/main/${roomKey}`, { method: 'GET' });
+            const roomData = await this.callRoom(roomKey, { method: 'GET' });
             roomsList.push(await roomData.json());
         }
-        console.log(roomsList);
-        return createResponse({ roomsList });
+        return this.createResponse({ roomsList });
     }
 }
 
