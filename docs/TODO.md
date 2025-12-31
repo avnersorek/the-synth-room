be39761eddc4fc97a5e18be704ee5f49a18601a7
Avner Sorek <Avner.Sorek@TikalK.com>
2025-12-31 18:02:24 +0200

    style

--- docs/TODO.md
+++ docs/TODO.md
@@ -3,5 +3,4 @@
 - space bar is not working well and there is a lot "selecting text" when playing with the platform
 
 # code
-- remove unused code
 - do a simple code review you'll find enough stuff like ...
--- webapp/src/ui/UI.ts
+++ webapp/src/ui/UI.ts
@@ -96,6 +96,7 @@ export class UI {
         </div>
         <div class="panel">
           <div class="controls">
+            <span class="section-label">you</span>
             <button id="play">
               <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="pointer-events: none;">
                 <path d="M20.4086 9.35258C22.5305 10.5065 22.5305 13.4935 20.4086 14.6474L7.59662 21.6145C5.53435 22.736 3 21.2763 3 18.9671L3 5.0329C3 2.72368 5.53435 1.26402 7.59661 2.38548L20.4086 9.35258Z" stroke="currentColor" stroke-width="1.5"/>
@@ -115,7 +116,9 @@ export class UI {
               </svg>
             </label>
           </div>
+          <div class="panel-divider"></div>
           <div class="bpm-panel">
+            <span class="section-label">room</span>
             <label for="bpm">BPM</label>
             <input type="number" id="bpm" value="120" min="40" max="240" />
           </div>
